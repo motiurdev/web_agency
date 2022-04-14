6de03135d22c69ff7ea3e6bfcc44613659d172d3
motiurdev <motiurrahmanmisu@gmail.com>
2022-04-14 21:43:43 +0600

    login page

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import loginImg from "../../../images/agency_website_4x.png";
+import "./Login.css";
 
 const Login = () => {
   return (
     <div>
-      <h1>Login Form</h1>
+      <Container>
+        <Row>
+          <Col sm={12} md={6}>
+            <img src={loginImg} className="img-fluid w-100" alt="login_img" />
+          </Col>
+          <Col sm={12} md={6}>
+            <form className="form">
+              <input
+                type="text"
+                id="email"
+                class="form__input"
+                autocomplete="off"
+                placeholder=" "
+              />
+              <label for="email" class="form__label">
+                Email
+              </label>
+            </form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
